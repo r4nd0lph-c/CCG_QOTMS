09f66e992ef5fedbe11177a7758a17afb3ba3a50
r4nd0lph <r11345roma@yandex.ru>
2024-12-29 22:56:02 +0300

    Update Data class & Create Language enum

--- Assets/Scripts/DataSystem/Data.cs
+++ Assets/Scripts/DataSystem/Data.cs
@@ -1,10 +1,19 @@
+using LanguageSystem;
+
 namespace DataSystem
 {
+    /// <summary>
+    /// This class represents the data used in the game.
+    /// It includes various settings and information that will be saved and loaded.
+    /// </summary>
     [System.Serializable]
     public class Data
     {
+        public Language gameLanguage;
+
         public Data()
         {
+            gameLanguage = Language.EN;
         }
     }
 }
---
+++ Assets/Scripts/LanguageSystem/Language.cs
@@ -0,0 +1,13 @@
+namespace LanguageSystem
+{
+    /// <summary>
+    /// Enum representing the available languages in the game.
+    /// Currently supported languages include English, Spanish, and Russian. 
+    /// </summary>
+    public enum Language
+    {
+        EN,
+        ES,
+        RU
+    }
+}
