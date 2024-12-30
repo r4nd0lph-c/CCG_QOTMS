e8f6b71f6e3555814ff75418bacc6c2b080a2955
r4nd0lph <r11345roma@yandex.ru>
2024-12-30 14:03:52 +0300

    Refactor files

--- Assets/Scripts/DataSystem/Data.cs
+++ Assets/Scripts/DataSystem/Data.cs
@@ -1,19 +1,14 @@
-using LanguageSystem;
-
 namespace DataSystem
 {
     /// <summary>
-    /// This class represents the data used in the game.
-    /// It includes various settings and information that will be saved and loaded.
+    /// Data class represents the data used in the game.
+    /// It includes various settings and information that will be saved and loaded using <see cref="DataManager"/>.
     /// </summary>
     [System.Serializable]
     public class Data
     {
-        public Language gameLanguage;
-
         public Data()
         {
-            gameLanguage = Language.EN;
         }
     }
 }
